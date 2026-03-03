ee4e7640a0f45639e42772c26ee4b73bb17d963d
jigsawrin <nuieseiun@gmail.com>
2026-03-04 01:52:32 +0900

    Emergency fix for constants.js syntax error (v3.3.7)

--- src/constants.js
+++ src/constants.js
@@ -19,4 +19,4 @@ export const MAP_WIDTH = 1280;
 export const MAP_HEIGHT = 720;
 export const CORE_X = 150;
 export const ENGAGEMENT_X = 240;
-export const GAME_VERSION = "3.3.6";
+export const GAME_VERSION = "3.3.7";
