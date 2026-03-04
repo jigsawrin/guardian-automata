902fdfcdc8a544f8e9562c172493cad953f84d29
jigsawrin <nuieseiun@gmail.com>
2026-03-04 11:03:07 +0900

    Increase late-game enemy speed x1.5 (v3.7.1)

--- src/constants.js
+++ src/constants.js
@@ -12,4 +12,4 @@ export const MAP_WIDTH = 1280;
 export const MAP_HEIGHT = 720;
 export const CORE_X = 150;
 export const ENGAGEMENT_X = 240;
-export const GAME_VERSION = "3.7.0";
+export const GAME_VERSION = "3.7.1";
